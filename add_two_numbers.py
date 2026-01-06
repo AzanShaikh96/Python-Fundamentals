df8b6ab2b16d074e2ee6dc682ee419ad08aff577
Shaikh Azan Shakeel <azanshaikh9678@gmail.com>
2026-01-06 20:20:25 +0500

    Add Python program to add two numbers

---
+++ add_two_numbers.py
@@ -0,0 +1,15 @@
+# Question: Write a Python program to add two numbers and display the result.
+
+# Solution 1 (Pre define variables)
+
+num1 = 5
+num2 = 10
+sum = num1 + num2
+print(f"The sum of given numbers {num1} and {num2} is: {sum}")
+
+
+# Solution 2 (User inputs)
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum = num1 + num2
+print(f"The sum of the provided numbers {num1} and {num2} is: {sum}")
